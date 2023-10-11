21cabadbdc6f7423b2884406f6ac6c4e1577333c
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-10-12 02:37:17 +1300

    got moving left and right working, forward and backward still need work

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-let cube = document.querySelector('.cube');
+let cube1 = document.getElementById('cube1')
+let cube2 = document.getElementById('cube2')
+/*
 let countX = 1;
 let countY = 1;
 let iterations = 0;
@@ -23,3 +25,68 @@ cube.addEventListener('mousemove', event => {
         prevMousePosY = mousePosY;
     }   
 });
+*/
+let wPressed = .9;
+let sPressed = .9;
+let dPressed = 20;
+let aPressed = -20;
+let timeForMove = true;
+function moveForward(){
+    timeForMove = false;
+    setTimeout(function(){
+        timeForMove = true;
+        wPressed+=.1
+        if(sPressed > 0){sPressed+=.1};
+        cube1.style.transform = `scale(${1 * wPressed}) translateX(${1 * aPressed}px)`
+        cube2.style.transform = `scale(${1 * wPressed}) translateX(${1 * aPressed}px)`
+    }, 100);
+}
+function moveBack(){
+    timeForMove = false;
+    setTimeout(function(){
+    timeForMove = true;
+    sPressed-=.1
+    if(wPressed > 0){wPressed-=.1};
+    cube1.style.transform = `scale(${1 * sPressed}) translateX(${1 * aPressed}px)`
+    cube2.style.transform = `scale(${1 * sPressed}) translateX(${1 * aPressed}px)`
+}, 100);
+}
+function moveRight(){
+    timeForMove = false;
+    setTimeout(function(){
+    timeForMove = true;
+    dPressed+=20;
+    aPressed = (dPressed);
+    cube1.style.transform = `translateX(${1 * dPressed}px)`
+    cube2.style.transform = `translateX(${1 * dPressed}px)`
+}, 100);
+}
+
+function moveLeft(){
+    timeForMove = false;
+    setTimeout(function(){
+    timeForMove = true;
+    aPressed-=20;
+    dPressed = (aPressed);
+    cube1.style.transform = `translateX(${1 * aPressed}px)`
+    cube2.style.transform = `translateX(${1 * aPressed}px)`
+}, 100);
+}
+
+document.addEventListener('keydown', event => {
+    if(event.code == 'KeyW'){
+        if(timeForMove == true)moveForward();
+    }
+    else if(event.code == 'KeyS'){
+        if(sPressed>0.21){
+            if(timeForMove == true)moveBack();
+    }
+    }
+    else if(event.code == 'KeyD'){
+            if(timeForMove == true)moveRight();
+    }
+    else if(event.code == 'KeyA'){
+            if(timeForMove == true)moveLeft();
+    }
+})
+
